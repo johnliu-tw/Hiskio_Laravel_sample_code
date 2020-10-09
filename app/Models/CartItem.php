c159b3e794c40c939f9599055d59033822931433
johnliu <johnliutw@hotmail.com>
2020-10-09 17:05:55 +0800

    `Cart Item` model

---
+++ app/Models/CartItem.php
@@ -0,0 +1,22 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+use Carbon\Carbon;
+
+class CartItem extends Model
+{
+    use HasFactory;
+
+
+    protected $fillable = ['quantity'];
+    protected $hidden = ['updated_at'];
+    protected $appends = ['current_price'];
+
+    public function getCurrentPriceAttribute()
+    {
+        return $this->quantity * 10;
+    }
+}
