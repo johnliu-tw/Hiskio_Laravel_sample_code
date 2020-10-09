2aa5e967c56ea467bbb1d2841d0403348ae303e4
johnliu <johnliutw@hotmail.com>
2020-10-09 13:51:48 +0800

    route init and fake product index

--- app/Providers/RouteServiceProvider.php
+++ app/Providers/RouteServiceProvider.php
@@ -18,6 +18,7 @@ class RouteServiceProvider extends ServiceProvider
      * @var string
      */
     public const HOME = '/home';
+    protected $namespace = 'App\Http\Controllers';
 
     /**
      * The controller namespace for the application.
