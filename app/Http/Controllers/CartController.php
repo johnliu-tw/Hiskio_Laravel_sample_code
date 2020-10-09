01bef14531e7cde008b50f20efe9711494cb6cdb
johnliu <johnliutw@hotmail.com>
2020-10-09 15:16:33 +0800

    `Cart`.`index` and timezone

--- app/Http/Controllers/CartController.php
+++ app/Http/Controllers/CartController.php
@@ -3,6 +3,7 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\DB;
 
 class CartController extends Controller
 {
@@ -13,7 +14,17 @@ class CartController extends Controller
      */
     public function index()
     {
-        //
+        $cart = DB::table('carts')->get()->first();
+        if (empty($cart)) {
+            DB::table('carts')->insert(['created_at' => now(), 'updated_at' => now()]);
+            $cart = DB::table('carts')->get()->first();
+        }
+        $carItems = DB::table('cart_items')->where('cart_id', $cart->id)
+                               ->get();
+        $cart = collect($cart);
+        $cart['items'] = collect($carItems);
+        
+        return response($cart);
     }
 
     /**
