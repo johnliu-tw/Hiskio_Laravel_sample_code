eb6573da1e10d016a1c82e6f55ee500c2aeb6429
johnliu <johnliutw@hotmail.com>
2020-10-18 16:53:51 +0800

    add web styling for `css`

--- resources/views/webs/index.blade.php
+++ resources/views/webs/index.blade.php
@@ -1,9 +1,18 @@
+<style>
+  body{
+    font-size: 16px;
+  }
+  .special-text{
+    text-align: center;
+    background-color: yellowgreen;
+  }
+</style>
 <div>
   <a href="/">商品列表</a>
   <a href="/contactUs">聯絡我們</a>
 </div>
-<h2>商品列表</h2>
-<img src="https://pbs.twimg.com/profile_images/737359467742912512/t_pzvyZZ_400x400.jpg" alt="">
+<h2 style="margin-top: 40px;">商品列表</h2>
+<img src="https://images.theconversation.com/files/350865/original/file-20200803-24-50u91u.jpg?ixlib=rb-1.1.0&q=45&auto=format&w=1200&h=1200.0&fit=crop" style="width: 300px;">
 <table>
   <thead>
     <tr>
@@ -15,14 +24,12 @@
   </thead>
   <tbody>
     @foreach( $products as $product )
-      @if ( $product->id != 1 )
-        <tr>
-          <td>{{ $product->title }}</td>
-          <td>{{ $product->content }}</td>
-          <td>{{ $product->price }}</td>
-          <td><a href="www.google.com">商品細節</a></td>
-        </tr>
-      @endif
+      <tr>
+        <td class="{{ $product->id == 1 ? 'special-text' : '' }}">{{ $product->title }}</td>
+        <td>{{ $product->content }}</td>
+        <td style="{{ $product->price < 600 ? 'color:red; font-size:22px' : ''  }}" >{{ $product->price }}</td>
+        <td><a href="www.google.com">商品細節</a></td>
+      </tr>
     @endforeach
   </tbody>
 </table>
