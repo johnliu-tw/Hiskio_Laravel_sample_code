9749a475cef3bf4f40835ae883368d18e5bc1efe
johnliu <johnliutw@hotmail.com>
2021-02-06 19:15:46 +0800

    C1 Plus - add seeder

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -3,6 +3,7 @@
 namespace Database\Seeders;
 
 use Illuminate\Database\Seeder;
+use App\Models\Product;
 
 class DatabaseSeeder extends Seeder
 {
@@ -13,6 +14,10 @@ class DatabaseSeeder extends Seeder
      */
     public function run()
     {
-        // \App\Models\User::factory(10)->create();
+        Product::create(['title' => '測試資料', 'content' => '測試內容', 'price' => rand(0, 300), 'quantity' => 20]);
+        Product::create(['title' => '測試資料2', 'content' => '測試內容2', 'price' => rand(0, 300), 'quantity' => 20]);
+        Product::create(['title' => '測試資料3', 'content' => '測試內容3', 'price' => rand(0, 300), 'quantity' => 20]);
+        $this->call(ProductSeeder::class);
+        $this->command->info('產生固定 product seeder');
     }
 }
---
+++ database/seeders/ProductSeeder.php
@@ -0,0 +1,22 @@
+<?php
+
+namespace Database\Seeders;
+
+use Illuminate\Database\Seeder;
+use App\Models\Product;
+
+class ProductSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        Product::upsert([
+            ['id' => 15, 'title' => '固定資料', 'content' => '固定內容', 'price' => 300, 'quantity' => 20],
+            ['id' => 16, 'title' => '固定資料2', 'content' => '固定內容2', 'price' => 500, 'quantity' => 20],
+        ], ['id'], ['price', 'quantity']);
+    }
+}
