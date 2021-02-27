4c88a87cf38230adeb2e87e5b8794e6bb292b8ce
johnliu <johnliutw@hotmail.com>
2021-02-28 04:09:10 +0800

    C2L2 plus - add Product replenish observer

---
+++ app/Notifications/ProductReplenish.php
@@ -0,0 +1,61 @@
+<?php
+
+namespace App\Notifications;
+
+use Illuminate\Bus\Queueable;
+use Illuminate\Contracts\Queue\ShouldQueue;
+use Illuminate\Notifications\Messages\MailMessage;
+use Illuminate\Notifications\Notification;
+
+class ProductReplenish extends Notification
+{
+    use Queueable;
+    protected $product;
+    /**
+     * Create a new notification instance.
+     *
+     * @return void
+     */
+    public function __construct($product)
+    {
+        $this->product = $product;
+    }
+
+    /**
+     * Get the notification's delivery channels.
+     *
+     * @param  mixed  $notifiable
+     * @return array
+     */
+    public function via($notifiable)
+    {
+        return ['database'];
+    }
+
+    /**
+     * Get the mail representation of the notification.
+     *
+     * @param  mixed  $notifiable
+     * @return \Illuminate\Notifications\Messages\MailMessage
+     */
+    public function toMail($notifiable)
+    {
+        return (new MailMessage)
+                    ->line('The introduction to the notification.')
+                    ->action('Notification Action', url('/'))
+                    ->line('Thank you for using our application!');
+    }
+
+    /**
+     * Get the array representation of the notification.
+     *
+     * @param  mixed  $notifiable
+     * @return array
+     */
+    public function toArray($notifiable)
+    {
+        return [
+            'msg' => '商品'.$this->product->title.'已經完成補貨'
+        ];
+    }
+}
---
+++ app/Observers/ProductObserver.php
@@ -0,0 +1,69 @@
+<?php
+
+namespace App\Observers;
+
+use App\Models\Product;
+use App\Notifications\ProductReplenish;
+
+class ProductObserver
+{
+    /**
+     * Handle the product "created" event.
+     *
+     * @param  \App\Models\Product  $product
+     * @return void
+     */
+    public function created(Product $product)
+    {
+        //
+    }
+
+    /**
+     * Handle the product "updated" event.
+     *
+     * @param  \App\Models\Product  $product
+     * @return void
+     */
+    public function updated(Product $product)
+    {
+        $changes = $product->getChanges();
+        if (isset($changes['quantity']) && $product->quantity > 0) {
+            foreach ($product->favorited_users as $user) {
+                $user->notify(new ProductReplenish($product));
+            }
+        }
+    }
+
+    /**
+     * Handle the product "deleted" event.
+     *
+     * @param  \App\Models\Product  $product
+     * @return void
+     */
+    public function deleted(Product $product)
+    {
+        //
+    }
+
+    /**
+     * Handle the product "restored" event.
+     *
+     * @param  \App\Models\Product  $product
+     * @return void
+     */
+    public function restored(Product $product)
+    {
+        //
+    }
+
+    /**
+     * Handle the product "force deleted" event.
+     *
+     * @param  \App\Models\Product  $product
+     * @return void
+     */
+    public function forceDeleted(Product $product)
+    {
+        //
+    }
+}
