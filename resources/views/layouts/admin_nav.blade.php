c95bc81648426667c3f27526ad443d7689949fae
johnliu <johnliutw@hotmail.com>
2021-03-18 22:04:34 +0800

    C3L1 plus - add custom navigation and custom component

--- resources/views/layouts/admin_nav.blade.php
+++ resources/views/layouts/admin_nav.blade.php
@@ -1,4 +1,78 @@
-<div class="container">
-  <a href="/admin/orders">訂單管理</a>
-  <a href="/admin/products">產品管理</a>
-</div>
+<ul class="navbar-nav bg-gradient-primary sidebar sidebar-dark accordion" id="accordionSidebar">
+
+  <!-- Sidebar - Brand -->
+  <a class="sidebar-brand d-flex align-items-center justify-content-center" href="index.html">
+      <div class="sidebar-brand-icon rotate-n-15">
+          <i class="fas fa-laugh-wink"></i>
+      </div>
+      <div class="sidebar-brand-text mx-3">Laravel 購物後台</div>
+  </a>
+
+  <!-- Divider -->
+  <hr class="sidebar-divider my-0">
+
+  <!-- Nav Item - Dashboard -->
+  <li class="nav-item active">
+      <a class="nav-link" href="/admin/orders">
+          <i class="fas fa-fw fa-tachometer-alt"></i>
+          <span>首頁</span></a>
+  </li>
+
+  <!-- Divider -->
+  <hr class="sidebar-divider">
+
+  <!-- Heading -->
+  <div class="sidebar-heading">
+      管理功能
+  </div>
+
+  <!-- Nav Item - Pages Collapse Menu -->
+  <li class="nav-item">
+      <a class="nav-link collapsed" href="#" data-toggle="collapse" data-target="#collapseTwo"
+          aria-expanded="true" aria-controls="collapseTwo">
+          <i class="fas fa-fw fa-cog"></i>
+          <span>訂單管理</span>
+      </a>
+      <div id="collapseTwo" class="collapse" aria-labelledby="headingTwo" data-parent="#accordionSidebar">
+          <div class="bg-white py-2 collapse-inner rounded">
+              <h6 class="collapse-header">清單</h6>
+              <a class="collapse-item" href="/admin/orders">訂單管理</a>
+              <a class="collapse-item" href="/admin/orders/datatables">訂單管理 DataTable 版</a>
+          </div>
+      </div>
+  </li>
+
+  <!-- Nav Item - Utilities Collapse Menu -->
+  <li class="nav-item">
+      <a class="nav-link collapsed" href="#" data-toggle="collapse" data-target="#collapseUtilities"
+          aria-expanded="true" aria-controls="collapseUtilities">
+          <i class="fas fa-fw fa-wrench"></i>
+          <span>產品管理</span>
+      </a>
+      <div id="collapseUtilities" class="collapse" aria-labelledby="headingUtilities"
+          data-parent="#accordionSidebar">
+          <div class="bg-white py-2 collapse-inner rounded">
+              <h6 class="collapse-header">清單</h6>
+              <a class="collapse-item" href="/admin/products">產品管理</a>
+          </div>
+      </div>
+  </li>
+
+
+  <!-- Nav Item - Charts -->
+  <li class="nav-item">
+      <a class="nav-link" href="charts.html">
+          <i class="fas fa-fw fa-chart-area"></i>
+          <span>圖表</span></a>
+  </li>
+
+
+  <!-- Divider -->
+  <hr class="sidebar-divider d-none d-md-block">
+
+  <!-- Sidebar Toggler (Sidebar) -->
+  <div class="text-center d-none d-md-inline">
+      <button class="rounded-circle border-0" id="sidebarToggle"></button>
+  </div>
+
+</ul>
