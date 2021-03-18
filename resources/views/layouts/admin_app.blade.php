2f95615014bb93966e99a47838cd74baf4bb6b71
johnliu <johnliutw@hotmail.com>
2021-03-18 22:03:47 +0800

    C3L1 plus - add template load

--- resources/views/layouts/admin_app.blade.php
+++ resources/views/layouts/admin_app.blade.php
@@ -2,16 +2,25 @@
     <head>
         <title>電子購物系統管理後台</title>
         <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css" integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossorigin="anonymous">
+        <link href="/vendor/fontawesome-free/css/all.min.css" rel="stylesheet" type="text/css">
+        <link
+            href="https://fonts.googleapis.com/css?family=Nunito:200,200i,300,300i,400,400i,600,600i,700,700i,800,800i,900,900i"
+            rel="stylesheet">
+        <link href="/css/sb-admin-2.min.css" rel="stylesheet">
         <script src="https://code.jquery.com/jquery-3.5.1.min.js" integrity="sha256-9/aliU8dGd2tb6OSsuzixeV4y/faTqgFtohetphbbj0=" crossorigin="anonymous"></script>
         <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.12.9/umd/popper.min.js" integrity="sha384-ApNbgh9B+Y1QKtv3Rn7W3mgPxhU9K/ScQsAP7hUibX39j7fakFPskvXusvfa0b4Q" crossorigin="anonymous"></script>
         <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/js/bootstrap.min.js" integrity="sha384-JZR6Spejh4U02d8jOt6vLEHfe/JQGiRRSQQxSfFWpi1MquVdAyjUar5+76PVCmYl" crossorigin="anonymous"></script>
         <script src="https://cdn.datatables.net/1.10.22/js/jquery.dataTables.min.js"></script>
         <link rel="stylesheet" href="/css/app.css">
     </head>
-    <body>
-        @include('layouts.admin_nav')
-        <div class="container">
-            @yield('content')
+    <body id="page-top">
+
+        <!-- Page Wrapper -->
+        <div id="wrapper">
+            @include('layouts.admin_nav')
+            <div id="content-wrapper" class="d-flex flex-column p-3 mt-3">
+                @yield('content')
+            </div>
         </div>
     </body>
     @stack('scripts')
